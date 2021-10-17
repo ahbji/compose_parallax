4e434350a53ad80b1df79d171f6dd2863c4377c1
andreaji <andrea.hb.ji@outlook.com>
2021-10-17 13:53:30 +0800

    Initial commit

---
+++ app/src/main/java/com/codingnight/parallax/ui/theme/Theme.kt
@@ -0,0 +1,18 @@
+package com.codingnight.parallax.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ParallaxTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) darkColors() else lightColors(),
+        content = content
+    )
+}
